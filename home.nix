e28b17987ece4e01893521874e26380760b26e4d
Maximilian Kapra <maximilian@kapra.de>
2024-07-05 12:58:55 +0200

    Add initial home-manager configuration with helix

---
+++ home.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, system, ... }:
+{
+  home.stateVersion = "23.11";
+  home.username = "mkapra";
+
+  programs.home-manager.enable = true;
+
+  programs.helix = {
+    enable = true;
+    defaultEditor = true;
+  }
+}
