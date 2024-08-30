6aca946c6811d635046858c15894e7004f53a41f
Maximilian Kapra <maximilian@kapra.de>
2024-08-30 08:53:48 +0200

    [nushell] Do not create .ssh/rc when nushell is enabled
    nushell has its own logic to create the SSH auth sock file in ~/.ssh.
    SSH overwrites this file again, when a new connection is established.

--- homeManagerModules/applications/ssh.nix
+++ homeManagerModules/applications/ssh.nix
@@ -1,21 +1,22 @@
-{ ... }:
+{ config, lib, ... }:
 let
   environmentVariables = {
     SSH_AUTH_SOCK = "~/.ssh/ssh_auth_sock";
   };
 in {
+  home.file.".ssh/rc".enable = !config.mkapraConfig.nushell.enable;
   home.file.".ssh/rc".text = ''
-    #!/bin/bash
-
-    # Fix SSH auth socket location so agent forwarding works with tmux.
-    if test "$SSH_AUTH_SOCK" ; then
-      ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
-    fi
+    #!/bin/bash                                                            
+                                                                       
+    # Fix SSH auth socket location so agent forwarding works with tmux.    
+    if test "$SSH_AUTH_SOCK" ; then                                        
+      ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock                           
+    fi                                                                     
   '';
 
   programs.nushell.extraEnv = ''
-    let sock = $"($env.HOME)/.ssh/ssh_auth_sock"
-    if ($env.SSH_AUTH_SOCK | str length) != 0 and ($env.SSH_AUTH_SOCK != $sock) {
+    let sock = $env.HOME | path join ".ssh/ssh_auth_sock"
+    if (not ($sock  | path exists)) {
       rm -f $sock
       ln -sf $env.SSH_AUTH_SOCK $sock
       $env.SSH_AUTH_SOCK = $sock
