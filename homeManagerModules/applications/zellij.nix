e9e6367e24308ad84e366da41b193460ec450456
Maximilian Kapra <maximilian@kapra.de>
2025-01-09 14:29:58 +0100

    [zellij] Remove bash integration and add theme

--- homeManagerModules/applications/zellij.nix
+++ homeManagerModules/applications/zellij.nix
@@ -1,6 +1,24 @@
 { ... }: {
   programs.zellij = {
     enable = true;
-    enableBashIntegration = true;
+    settings = {
+      themes = {
+        everforest-light-custom = {
+          bg = "#fdf6e3";
+          black = "#fdf6e3";
+          blue = "#3a94c5";
+          cyan = "#35a77c";
+          fg = "#5c6a72";
+          green = "#8da101";
+          magenta = "#df69ba";
+          orange = "#f57d26";
+          red = "#f85552";
+          white = "#5c6a72";
+          yellow = "#dfa000";
+        };
+      };
+      theme = "everforest-light-custom";
+      default_layout = "compact";
+    };
   };
 }
