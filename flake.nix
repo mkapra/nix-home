a451b0337a41ac74e964736e783f4bd01a531df1
Maximilian Kapra <maximilian@kapra.de>
2025-01-10 09:05:44 +0100

    Add nixpkgs inputs
    * pkgs-stable, -unstable and -deprecated
    * Use unstable nixpkgs for helix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
-  outputs = { self }: {
+  inputs.nixpkgs.url = "nixpkgs/nixos-24.11";
+  inputs.nixpkgs-deprecated.url = "nixpkgs/nixos-24.05";
+  inputs.nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+
+  outputs = { self }: let
+    system = "x86_64-linux";
+    pkgs = inputs.nixpkgs.legacyPackages.${system};
+    pkgs-deprecated = inputs.nixpkgs-deprecated.legacyPackages.${system};
+    pkgs-unstable = inputs.nixpkgs-unstable.legacyPackages.${system};
+  in {
     homeManagerModules.default = self.homeManagerModules.mkapra-home;
-    homeManagerModules.mkapra-home = import ./home.nix;
+    homeManagerModules.mkapra-home = import ./home.nix { inherit pkgs pkgs-deprecated pkgs-unstable; };
   };
 }
