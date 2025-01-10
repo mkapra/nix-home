609ec88c702019094285572526e92623600f64d9
Maximilian Kapra <maximilian@kapra.de>
2025-01-10 09:27:10 +0100

    Remove inputs

--- flake.nix
+++ flake.nix
@@ -1,18 +1,8 @@
 {
   description = "mkapra's personal home-manager configuration";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-24.11";
-  inputs.nixpkgs-deprecated.url = "nixpkgs/nixos-24.05";
-  inputs.nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
-
-  outputs = inputs@{ self, ... }:
-  let
-    system = "x86_64-linux";
-    pkgs = inputs.nixpkgs.legacyPackages.${system};
-    pkgs-deprecated = inputs.nixpkgs-deprecated.legacyPackages.${system};
-    pkgs-unstable = inputs.nixpkgs-unstable.legacyPackages.${system};
-  in {
+  outputs = { self, ... }: {
     homeManagerModules.default = self.homeManagerModules.mkapra-home;
-    homeManagerModules.mkapra-home = import ./home.nix { inherit pkgs pkgs-deprecated pkgs-unstable system config; };
+    homeManagerModules.mkapra-home = import ./home.nix;
   };
 }
