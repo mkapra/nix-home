15ee2b7e0fdc5aa394c4df5da38486a7a5e5ea64
Maximilian Kapra <maximilian@kapra.de>
2025-01-10 09:12:07 +0100

    Fix missing arguments for inputs

--- flake.nix
+++ flake.nix
@@ -1,9 +1,12 @@
 {
+  description = "mkapra's personal home-manager configuration";
+
   inputs.nixpkgs.url = "nixpkgs/nixos-24.11";
   inputs.nixpkgs-deprecated.url = "nixpkgs/nixos-24.05";
   inputs.nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
 
-  outputs = inputs@{ self }: let
+  outputs = inputs@{ self, ... }:
+  let
     system = "x86_64-linux";
     pkgs = inputs.nixpkgs.legacyPackages.${system};
     pkgs-deprecated = inputs.nixpkgs-deprecated.legacyPackages.${system};
