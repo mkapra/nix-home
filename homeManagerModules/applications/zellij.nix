5d7dfca7f501f65f3a51f09ab8d358448fc514ca
Maximilian Kapra <maximilian_kapra@genua.de>
2025-01-13 21:11:22 +0100

    [zellij] Convert zellij configuration to home file

--- homeManagerModules/applications/zellij.nix
+++ homeManagerModules/applications/zellij.nix
@@ -1,30 +1,38 @@
 { ... }: {
   programs.zellij = {
     enable = true;
-    settings = {
-      keybinds = {
-        unbind = "Ctrl o";
-        "shared_except \"session\" \"locked\"" = {
-          "bind \"Ctrl Alt o\"" = { SwitchToMode = "Session"; };
-        };
-      };
-      themes = {
-        everforest-light-custom = {
-          bg = "#fdf6e3";
-          black = "#fdf6e3";
-          blue = "#3a94c5";
-          cyan = "#35a77c";
-          fg = "#5c6a72";
-          green = "#8da101";
-          magenta = "#df69ba";
-          orange = "#f57d26";
-          red = "#f85552";
-          white = "#5c6a72";
-          yellow = "#dfa000";
-        };
-      };
-      theme = "everforest-light-custom";
-      default_layout = "compact";
-    };
   };
+
+  home.file.".config/zellij/config.kdl".source = ''
+theme "everforest-light-custom"
+default_layout "compact"
+
+keybinds {
+	shared_except "session" "locked" {
+		bind "Ctrl Alt f" {
+			ToggleFloatingPanes;
+		}
+		bind "Ctrl Alt o" {
+			SwitchToMode "Session"
+		}
+	}
+	unbind "Ctrl o" "Alt o" "Alt f"
+}
+
+themes {
+	everforest-light-custom {
+		bg "#fdf6e3"
+		black "#fdf6e3"
+		blue "#3a94c5"
+		cyan "#35a77c"
+		fg "#5c6a72"
+		green "#8da101"
+		magenta "#df69ba"
+		orange "#f57d26"
+		red "#f85552"
+		white "#5c6a72"
+		yellow "#dfa000"
+	}
+}
+  '';
 }
