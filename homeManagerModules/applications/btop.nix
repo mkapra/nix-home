00653955c8f8147b369e677ddf00976468424759
Maximilian Kapra <maximilian@kapra.de>
2025-01-22 15:19:36 +0100

    [btop] Change color theme

--- homeManagerModules/applications/btop.nix
+++ homeManagerModules/applications/btop.nix
@@ -1,16 +1,8 @@
 { ... }: {
-  home.file."btoptheme" = {
-    source = builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/rose-pine/btop/main/rose-pine-dawn.theme";
-      sha256 = "9c5863fefe2ccce52e06efc0c9d7a6f1f75387e0dec50452bb0bbb56266743ac";
-    };
-    target = ".config/btop/themes/rosepine_dawn.theme";
-  };
-
   programs.btop = {
     enable = true;
     settings = {
-      color_theme = "rosepine_dawn";
+      color_theme = "everforest-light-medium";
     };
   };
 }
