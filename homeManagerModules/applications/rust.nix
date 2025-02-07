42e5f11acb4cb662b1de4e7bff5f0a9c8bca4203
Maximilian Kapra <maximilian@kapra.de>
2025-02-07 13:22:04 +0100

    Install rust only when mkapra-home.rust.enable is set to true

--- homeManagerModules/applications/rust.nix
+++ homeManagerModules/applications/rust.nix
@@ -1,20 +1,24 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [
-    gcc
-    rustc
-    cargo
-    rust-analyzer
-    rustfmt
-    clippy
-  ];
+{ pkgs, lib, ... }: {
+  options.mkapra-home.rust.enable = lib.mkEnableOption "install rust";
 
-  programs.helix.languages = {
-    language = [{
-      name = "rust";
-      auto-format = true;
-      formatter = {
-        command = "rustfmt";
-      };
-    }];
+  config = lib.mkIf mkapra-home.rust.enable {
+    home.packages = with pkgs; [
+      gcc
+      rustc
+      cargo
+      rust-analyzer
+      rustfmt
+      clippy
+    ];
+  
+    programs.helix.languages = {
+      language = [{
+        name = "rust";
+        auto-format = true;
+        formatter = {
+          command = "rustfmt";
+        };
+      }];
+    };
   };
 }
