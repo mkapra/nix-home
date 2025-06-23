f19acd7d2017c786ecefa93998f3f014a7876b8f
Maximilian Kapra <maximilian_kapra@genua.de>
2025-06-23 15:41:02 +0200

    ssh: Make rc file executable

--- homeManagerModules/applications/ssh.nix
+++ homeManagerModules/applications/ssh.nix
@@ -4,14 +4,17 @@ let
     SSH_AUTH_SOCK = "$HOME/.ssh/ssh_auth_sock";
   };
 in {
-  home.file.".ssh/rc".text = ''
-    #!/bin/bash
+  home.file.".ssh/rc" = {
+    executable = true;
+    text = ''
+      #!/bin/bash
 
-    # Fix SSH auth socket location so agent forwarding works with tmux.    
-    if test "$SSH_AUTH_SOCK" ; then
-      ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
-    fi
-  '';
+      # Fix SSH auth socket location so agent forwarding works with tmux.    
+      if test "$SSH_AUTH_SOCK" ; then
+        ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
+      fi
+    '';
+  };
 
   programs.nushell.extraEnv = ''
     let sock = $env.HOME | path join ".ssh/ssh_auth_sock"
