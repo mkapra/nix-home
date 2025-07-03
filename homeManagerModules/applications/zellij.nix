4864f08a72717d40ea029f9b6cb1fc0edb77484f
Maximilian Kapra <maximilian_kapra@genua.de>
2025-07-03 09:53:53 +0200

    zellij: Convert zellij theme

--- homeManagerModules/applications/zellij.nix
+++ homeManagerModules/applications/zellij.nix
@@ -4,21 +4,128 @@
   home.file.".config/zellij/config.kdl".text = ''
 default_shell "nu"
 theme "everforest-light-custom"
+'';
 
+  home.file.".config/zellij/themes/everforest-light.kdl".text = ''
 themes {
-	everforest-light-custom {
-		bg "#fdf6e3"
-		black "#fdf6e3"
-		blue "#3a94c5"
-		cyan "#35a77c"
-		fg "#5c6a72"
-		green "#8da101"
-		magenta "#df69ba"
-		orange "#f57d26"
-		red "#f85552"
-		white "#5c6a72"
-		yellow "#dfa000"
-	}
+    everforest-light-custom {
+        text_unselected {
+            base 92 106 114
+            background 239 235 212
+            emphasis_0 245 125 38
+            emphasis_1 53 167 124
+            emphasis_2 141 161 1
+            emphasis_3 223 105 186
+        }
+        text_selected {
+            base 92 106 114
+            background 239 235 212
+            emphasis_0 245 125 38
+            emphasis_1 53 167 124
+            emphasis_2 141 161 1
+            emphasis_3 223 105 186
+        }
+        ribbon_selected {
+            base 239 235 212
+            background 141 161 1
+            emphasis_0 248 85 82
+            emphasis_1 245 125 38
+            emphasis_2 223 105 186
+            emphasis_3 58 148 197
+        }
+        ribbon_unselected {
+            base 239 235 212
+            background 92 106 114
+            emphasis_0 248 85 82
+            emphasis_1 92 106 114
+            emphasis_2 58 148 197
+            emphasis_3 223 105 186
+        }
+        table_title {
+            base 92 106 114
+            background 253 246 227
+            emphasis_0 245 125 38
+            emphasis_1 53 167 124
+            emphasis_2 141 161 1
+            emphasis_3 223 105 186
+        }
+        table_cell_selected {
+            base 92 106 114
+            background 239 235 212
+            emphasis_0 245 125 38
+            emphasis_1 53 167 124
+            emphasis_2 141 161 1
+            emphasis_3 223 105 186
+        }
+        table_cell_unselected {
+            base 92 106 114
+            background 239 235 212
+            emphasis_0 245 125 38
+            emphasis_1 53 167 124
+            emphasis_2 141 161 1
+            emphasis_3 223 105 186
+        }
+        list_selected {
+            base 92 106 114
+            background 239 235 212
+            emphasis_0 245 125 38
+            emphasis_1 53 167 124
+            emphasis_2 141 161 1
+            emphasis_3 223 105 186
+        }
+        list_unselected {
+            base 92 106 114
+            background 239 235 212
+            emphasis_0 245 125 38
+            emphasis_1 53 167 124
+            emphasis_2 141 161 1
+            emphasis_3 223 105 186
+        }
+        frame_selected {
+            base 141 161 1
+            background 0
+            emphasis_0 245 125 38
+            emphasis_1 53 167 124
+            emphasis_2 223 105 186
+            emphasis_3 0
+        }
+        frame_highlight {
+            base 245 125 38
+            background 0
+            emphasis_0 245 125 38
+            emphasis_1 245 125 38
+            emphasis_2 245 125 38
+            emphasis_3 245 125 38
+        }
+        exit_code_success {
+            base 141 161 1
+            background 0
+            emphasis_0 53 167 124
+            emphasis_1 239 235 212
+            emphasis_2 223 105 186
+            emphasis_3 58 148 197
+        }
+        exit_code_error {
+            base 248 85 82
+            background 0
+            emphasis_0 223 160 0
+            emphasis_1 0
+            emphasis_2 0
+            emphasis_3 0
+        }
+        multiplayer_user_colors {
+            player_1 223 105 186
+            player_2 58 148 197
+            player_3 0
+            player_4 223 160 0
+            player_5 53 167 124
+            player_6 0
+            player_7 248 85 82
+            player_8 0
+            player_9 0
+            player_10 0
+        }
+    }
 }
   '';
 }
