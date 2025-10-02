50feb8486c7fc18ddc3f0971b4caea0d1e3dbf18
Maximilian Kapra <maximilian@kapra.de>
2025-10-02 07:45:55 +0200

    Remove devenv from default home.nix

--- home.nix
+++ home.nix
@@ -1,12 +1,8 @@
-{ pkgs-unstable, ... }:
+{ ... }:
 {
   home.username = "mkapra";
 
   imports = [ ./homeManagerModules ];
-
-  home.packages = [
-    pkgs-unstable.devenv
-  ];
 
   programs.home-manager.enable = true;
 }
