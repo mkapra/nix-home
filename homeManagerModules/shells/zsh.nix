7ffa694c2a77ef8473235c6f350bf65fb9d53c99
Maximilian Kapra <maximilian@kapra.de>
2025-10-02 08:34:59 +0200

    zsh: refactor config

--- homeManagerModules/shells/zsh.nix
+++ homeManagerModules/shells/zsh.nix
@@ -6,54 +6,27 @@
     autosuggestion.enable = true;
 
     initContent = ''
-      bindkey '^p' history-search-backward
-      bindkey '^n' history-search-forward
-
-      zstyle ':prompt:pure:cyan' color #3A94C5
-      zstyle ':prompt:pure:red' color #F85552
-      zstyle ':prompt:pure:prompt:success' color cyan
-
       zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
       zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
 
-      # Enable vcs_info for Git
-      autoload -Uz vcs_info
-
-      # Git-only configuration
-      zstyle ':vcs_info:*' enable git
-      zstyle ':vcs_info:git:*' check-for-changes true
-      zstyle ':vcs_info:git:*' unstagedstr " %F{#DFA000}%f "
-      zstyle ':vcs_info:git:*' stagedstr " %F{#8DA101}%f "
-      zstyle ':vcs_info:git:*' formats '󰘬 %b%u%c'
-      zstyle ':vcs_info:git:*' actionformats '  󰘬 %b%u%c'
-
-      # Update vcs_info and set prompts before each prompt render
-      precmd() {
+      precmd () {
         vcs_info
-        set_left_prompt
-        set_right_prompt
+        print -Pn "\e]2;%-3~\a";
       }
 
-      # Set left prompt with dir and colored ❯ based on last command
-      set_left_prompt() {
-        local last_status=$?
-        # local dir="''${PWD##*/}"
-	local dir="%(4~|.../%3~|%~)"
+      autoload -Uz vcs_info
+      zstyle ':vcs_info:*' enable git
+      zstyle ':vcs_info:*' actionformats '%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f'
+      zstyle ':vcs_info:*' formats '%F{5}[%F{2}%b%F{5}]%f'
+      zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
 
-        if [[ $last_status -eq 0 ]]; then
-          PROMPT="''${dir} %F{#3a94c5}❯%f "
-        else
-          PROMPT="''${dir} %F{#f85552}❯%f "
-        fi
-      }
-
-      # Set right prompt to Git info (via vcs_info)
-      set_right_prompt() {
-	RPROMPT="''${vcs_info_msg_0_}"
-      }
+      setopt prompt_subst
+      PS1='%F{blue}%B%~%b%f %F{%(?.green.red)}❯%f '
+      RPS1='%F{cyan}''${vcs_info_msg_0_}%f'
     '';
 
     history = {
+      path = "${config.xdg.dataHome}/.history";
       append = true;
       findNoDups = true;
       ignoreAllDups = true;
